258f2ecec4fe65a9b9fc804fb4f8d91d933f38a4
reserve85 <111107925+reserve85@users.noreply.github.com>
2023-11-30 08:22:39 +0100

    Add files via upload
    nochmal Write-Rechte Fix...

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ fi
 
 if [ ! -f "${SCRIPT_DIR}/HoymilesZeroExport_Config_Override.ini" ]; then
     touch "${SCRIPT_DIR}/HoymilesZeroExport_Config_Override.ini"
-	chmod +rw "$SCRIPT_DIR/HoymilesZeroExport_Config_Override.ini"
+	chmod u=rw,g=rw,o=rw "$SCRIPT_DIR/HoymilesZeroExport_Config_Override.ini"
 fi
 
 cat << EOF | tee /etc/systemd/system/HoymilesZeroExport.service
