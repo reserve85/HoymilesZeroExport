fba1775c94b2d08e43dd2a5d60fda29834bb5dab
reserve85 <111107925+reserve85@users.noreply.github.com>
2024-04-20 19:51:14 +0200

    Add files via upload

--- update.sh
+++ update.sh
@@ -4,11 +4,13 @@ if [ $# -gt 1 ]; then
   echo "Error: Too many arguments"
 elif [ $# -eq 1 ]; then
   # Use custom URL if provided
-  DOWNLOAD_URL="$1"
+  BRANCH="$1"
 else
   # Default URL if no argument provided
-  DOWNLOAD_URL="https://github.com/reserve85/HoymilesZeroExport/archive/refs/heads/main.zip"
+  BRANCH="main"
 fi
+BASE_URL="https://github.com/reserve85/HoymilesZeroExport/archive/refs/heads/"
+DOWNLOAD_URL="$BASE_URL$BRANCH.zip"
 
 # Change directory to the script's directory
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
@@ -16,17 +18,17 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 echo "Start update of HoymilesZeroExport"
 
 # Download and unzip the file
-wget "$DOWNLOAD_URL" -O main.zip
-unzip main.zip
-rm main.zip
+wget "$DOWNLOAD_URL" -O $BRANCH.zip
+unzip $BRANCH.zip
+rm $BRANCH.zip
 
 if [ $(dpkg-query -W -f='${Status}' rsync 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
   apt-get install rsync;
 fi
 
-rsync -a HoymilesZeroExport-main/ ./
-rm -r HoymilesZeroExport-main/
+rsync -a HoymilesZeroExport-$BRANCH/ ./
+rm -r HoymilesZeroExport-$BRANCH/
 
 chmod +x $SCRIPT_DIR/install.sh
 chmod +x $SCRIPT_DIR/HoymilesZeroExport.py
