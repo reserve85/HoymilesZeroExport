e57876cd02d0153048f6b9d5a48fb08def4de50e
reserve85 <reserve85@googlemail.com>
2024-10-07 21:07:45 +0200

    Update HoymilesZeroExport.py
    second try to fix json-bool value

--- HoymilesZeroExport.py
+++ HoymilesZeroExport.py
@@ -1085,7 +1085,7 @@ class OpenDTU(DTU):
             logger.info('OpenDTU: Inverter "%s": Turn on',NAME[pInverterId])
         else:
             logger.info('OpenDTU: Inverter "%s": Turn off',NAME[pInverterId])
-        mySendStr = f'''data={{"serial":"{SERIAL_NUMBER[pInverterId]}", "power":{pActive}}}'''
+        mySendStr = f'''data="serial":"{SERIAL_NUMBER[pInverterId]}", "power":{json.dumps(pActive)}'''     
         response = self.GetResponseJson('/api/power/config', mySendStr)
         if response['type'] != 'success':
             raise Exception(f"Error: SetPowerStatus error: {response['message']}")
@@ -1555,6 +1555,8 @@ if config.has_section("MQTT_CONFIG"):
 try:
     logger.info("---Init---")
     newLimitSetpoint = 0
+
+    DTU.SetPowerStatus(1, True)
     DTU.CheckMinVersion()
     if GetHoymilesAvailable():
         for i in range(INVERTER_COUNT):
